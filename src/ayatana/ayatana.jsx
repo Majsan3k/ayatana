39618084a6c24b94ad141e7e36e63341512488a0
Maja Lund <maja.lund@live.se>
2021-07-05 14:34:33 +0200

    add some info about the word ayatana

--- src/ayatana/ayatana.jsx
+++ src/ayatana/ayatana.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import './ayatana.css'
 
  const Ayatana = () => {
      return (
-         <div>
+         <div className="ayatana-container">
              <img style={{width:"100%"}} src='./img/buddah.jpg' />
              <div>Ayatana</div>
+
+             <div className="ayatana-desc">
+                 {`Ayatana har sitt ursprung i den buddhistiska filosofin. Det finns ingen direkt översättning till varken svenska eller engelska men ordet kan beskrivas som sinnessfär. Inom buddhismen brukar det sägas att det finns 12 Ayatanas, vilka är grunden till vårt medvetande. Dessa 12 innefattar både de sinnen vi besitter, interna sinnesbaser, och de objekt vi kan uppleva med våra sinnen, externa sinnesbaser. Tillsammans skapas sex par av interna-externa sinnesbaser:
+                 `}
+                 <ul className="ayatana-list">
+                    <li>ögat och objekt vi ser​ </li>
+                    <li>örat och ljud</li>
+                    <li>näsan och lukter</li>
+                    <li>tungan och smaker</li>
+                    <li>kroppen och beröring</li>
+                    <li>själen och mentala objekt som påverkas oss, till exempel känslor</li>
+                 </ul>
+             </div>
          </div>
      )
  }

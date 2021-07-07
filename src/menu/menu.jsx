ee1ea971d87306842a9efa4dccc2d7b1823bdd67
Jens <cakism@users.noreply.github.com>
2021-07-07 11:35:57 +0200

    Added responsive menu with hamburger icon from font-awesome. Added dependency to styled-components for responsive menu layout. Maybe some more fixes needed to fix menu styling?

--- src/menu/menu.jsx
+++ src/menu/menu.jsx
@@ -1,16 +1,45 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react'
+import { NavLink } from 'react-router-dom';
 import './menu.css'
+import { Navigation } from "./HamburgerMenu";
 
 const Menu = () => {
+
+    const [isExpanded, setIsExpanded] = useState(false);
+
+    function toggleMenu() {
+        setIsExpanded(!isExpanded);
+    }
+
     return (
         <div className="menuBar">
             <div className="menuItems">
-                <Link className="link" to="/">Home</Link>
-                <Link className="link" to="/tjanster">Tjänster</Link>
-                <Link className="link" to="/ayatana">Vad betyder Ayatana?</Link>
-                <Link className="link" to="/om-mig">Om mig</Link>
-                <Link className="link" to="/kontakt">Kontakt</Link>
+                <Navigation>
+                    <nav className="nav">
+                        <i
+                            className="fa fa-bars"
+                            aria-hidden="true"
+                            onClick={toggleMenu}
+                        />
+                        <ul className={`collapsed ${isExpanded ? "is-expanded" : ""}`}>
+                            <NavLink activeClassName="active" to="/">
+                                <li>Home</li>
+                            </NavLink>
+                            <NavLink activeClassName="active" to="/tjanster">
+                                <li>Tjänster</li>
+                            </NavLink>
+                            <NavLink activeClassName="active" to="/ayatana">
+                                <li>Vad betyder Ayatana?</li>
+                            </NavLink>
+                            <NavLink activeClassName="active" to="/om-mig">
+                                <li>Om mig</li>
+                            </NavLink>
+                            <NavLink activeClassName="active" to="/kontakt">
+                                <li>Kontakt</li>
+                            </NavLink>
+                        </ul>
+                    </nav>
+                </Navigation>
             </div>
         </div>
     )

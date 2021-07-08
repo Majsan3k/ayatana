9089aad271e9a4619eaec22bbddebca6d840884f
Maja Lund <maja.lund@live.se>
2021-07-08 10:16:54 +0200

    added loading spinner while sending email and confirmation that it has been sent

--- src/contact/contactForm.jsx
+++ src/contact/contactForm.jsx
@@ -1,28 +1,44 @@
-import React from 'react';
+import { React, useState} from 'react';
 import emailjs from 'emailjs-com';
 
 import './contactForm.css'
 
 const ContactForm = () => {
 
+  const [emailStatus, setEmailStatus] = useState('');
+  const [sendingEmail, setSendingEmail] = useState(false);
+
   const sendEmail = (e) => {
     e.preventDefault();
+    setSendingEmail(true);
+
+    setEmailStatus("Ditt mail skickas nu med ljusets hastighet")
 
     emailjs.sendForm('service_lj5200b', 'template_ztaayy8', e.target, 'user_Y6VHJEF9OLg64vSxsWdxm')
       .then((result) => {
-          console.log(result.text);
+        e.target.reset();
+        setEmailStatus("Tack för ditt mail! Jag önskar dig en underbar dag!")
+        console.log(result.text);
       }, (error) => {
-          console.log(error.text);
-      });
+        setEmailStatus("Någonting gick fel, försök skicka mailet igen")
+        console.log(error.text);
+      })
+      .then(() => setSendingEmail(false));
   }
 
   return (
-    <form className="contact-form" onSubmit={sendEmail}>
-      <input placeholder="Namn" type="text" name="name" />
-      <input placeholder="Email" type="email" name="email" />
-      <textarea placeholder="Skriv ditt meddelande här" name="message" />
-      <button className="send-btn" type="submit" value="Skicka">Skicka</button>
-    </form>
+    <div className="email-container">
+      <form className="contact-form" onSubmit={sendEmail}>
+        <input placeholder="Namn" type="text" name="name" />
+        <input placeholder="Email" type="email" name="email" />
+        <textarea placeholder="Skriv ditt meddelande här" name="message" />
+        <button className="send-btn" type="submit" value="Skicka" disabled={sendingEmail}>
+          Skicka {sendingEmail && <i className="fa fa-refresh fa-spin"></i>} 
+
+        </button>
+      </form>
+      <span>{emailStatus}</span>
+    </div>
   );
 }
 

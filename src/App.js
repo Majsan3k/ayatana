d5842a9b11f6cf7bba2b371960ce99ea9d9b8508
Maja Lund <maja.lund@live.se>
2021-07-12 12:03:39 +0200

    Added basename to React Router to make it compatible with GH pages

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import Contact from './contact/contact';
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <div className="App">
         <Header />
         <Menu />

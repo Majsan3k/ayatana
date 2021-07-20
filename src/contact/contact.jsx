68622457d4520bc3feb7b5ed8ba212b312448e8d
Maja Lund <maja.lund@live.se>
2021-07-20 11:51:04 +0200

    Updated styling on map and removed unused className

--- src/contact/contact.jsx
+++ src/contact/contact.jsx
@@ -8,7 +8,7 @@ const Contact = () => {
   return (
     <div className="contact-container">
       <img className="image" src={forest} />
-      <div className="container reversed contact-details">
+      <div className="container reversed">
         <ContactForm />
         <span className="half-screen container-item">
           {`Letar du efter ett mer holistiskt och hållbart förhållningssätt till din hälsa? Vill du njuta av en härlig behandling? Eller undrar du något helt annat? Tveka inte att höra av dig! 
@@ -17,7 +17,6 @@ const Contact = () => {
         </span>
       </div>
       <MapContainer/>
-
     </div>
   )
 }
--- src/contact/mapContainer.jsx
+++ src/contact/mapContainer.jsx
@@ -1,7 +1,7 @@
 
 const MapContainer = (props) => (
-  <div>
-    <iframe width="100%" height="400" frameborder="0" scrolling="yes" id="gmap_canvas" 
+  <div className="map-container">
+    <iframe width="95%" height="400" frameborder="0" scrolling="yes" id="gmap_canvas" 
     src="https://maps.google.com/maps?width=520&amp;height=400&amp;hl=en&amp;q=Gotlandsgatan%2044%20Stockholm+(Behandlingsrum)&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">
       </iframe>
     <script type='text/javascript' src='https://embedmaps.com/google-maps-authorization/script.js?id=c7286231a89907fbc8a6e15ec45c820685bfb8c4'></script>

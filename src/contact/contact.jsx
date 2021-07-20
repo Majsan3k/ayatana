fb8f39b19876803f3ebf60b2d5f83698571b8c13
Maja Lund <maja.lund@live.se>
2021-07-20 11:43:52 +0200

    Added map to contact page

--- src/contact/contact.jsx
+++ src/contact/contact.jsx
@@ -2,21 +2,24 @@ import React from 'react'
 import './contact.css'
 import ContactForm from './contactForm'
 import forest from '../img/forest.jpg'
+import MapContainer from './mapContainer.jsx'
 
- const Contact = () => {
-     return (
-         <div className="contact-container">
-           <img className="image" src={forest} />
-           <div className="container reversed">
-              <ContactForm />
-              <span className="half-screen container-item">
-              {`Letar du efter ett mer holistiskt och hållbart förhållningssätt till din hälsa? Vill du njuta av en härlig behandling? Eller undrar du något helt annat? Tveka inte att höra av dig! 
+const Contact = () => {
+  return (
+    <div className="contact-container">
+      <img className="image" src={forest} />
+      <div className="container reversed contact-details">
+        <ContactForm />
+        <span className="half-screen container-item">
+          {`Letar du efter ett mer holistiskt och hållbart förhållningssätt till din hälsa? Vill du njuta av en härlig behandling? Eller undrar du något helt annat? Tveka inte att höra av dig! 
 
               Fyll i formuläret eller skicka ett mail till direkt till mig på info@ayatana.se.`}
-              </span>
-           </div>
-         </div>
-     )
- }
+        </span>
+      </div>
+      <MapContainer/>
 
- export default Contact;
+    </div>
+  )
+}
+
+export default Contact;

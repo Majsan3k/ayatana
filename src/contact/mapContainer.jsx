85f600fead5a9ad5a4185b512ce7adaa57c0a91f
Maja Lund <maja.lund@live.se>
2021-07-20 11:44:27 +0200

    Added mapContainer.jsx

---
+++ src/contact/mapContainer.jsx
@@ -0,0 +1,12 @@
+
+const MapContainer = (props) => (
+  <div>
+    <iframe width="100%" height="400" frameborder="0" scrolling="yes" id="gmap_canvas" 
+    src="https://maps.google.com/maps?width=520&amp;height=400&amp;hl=en&amp;q=Gotlandsgatan%2044%20Stockholm+(Behandlingsrum)&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">
+      </iframe>
+    <script type='text/javascript' src='https://embedmaps.com/google-maps-authorization/script.js?id=c7286231a89907fbc8a6e15ec45c820685bfb8c4'></script>
+  </div>
+
+)
+
+export default MapContainer

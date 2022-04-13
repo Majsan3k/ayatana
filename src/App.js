84f93d2347f515ddd82fc78cc316b26cecf12a77
Maja Lund <maja.lund@live.se>
2022-04-13 11:24:44 +0200

    Added Banner

--- src/App.js
+++ src/App.js
@@ -9,10 +9,15 @@ import './shared.css'
 import Footer from './footer/footer';
 import Ayatana from './ayatana/ayatana';
 import Contact from './contact/contact';
+import Banner from 'react-js-banner';
 
 function App() {
   return (
     <Router>
+      <Banner 
+        title="OBS! Företaget är för tillfället under en välbehövlig vila. Ta hand om er så hörs vi i framtiden! Namaste"
+        css={{marginBottom:"5px"}}
+      />
       <div className="App">
         <Header />
         <Menu />
